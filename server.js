31931d59b7b27f6c18f4db0d224a23f33af501e2
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2016-11-06 12:43:03 -0500

    add project png. add Twit post function.

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+var Twit = require('twit')
+
 var fs = require('fs'),
     path = require('path'),
     Twit = require('twit'),
     config = require(path.join(__dirname, 'config.js'));
+
+var T = new Twit(config);
+
+T.post('statuses/update', { status: 'Look, I am tweeting!' }, function(err, data, response) {
+  console.log(data)
+});
